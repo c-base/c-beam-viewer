17ffdf5a1b8231e3ceb0fbf06bb0553b6a33781f
cmile <github@cmile.de>
2014-08-18 22:12:48 +0200

    added SettingsActivity
    added SettingsActivity
    
    added SettingsActivity
    
    added SettingsActivity

---
+++ app/src/main/java/org/c_base/c_beam_viewer/SettingsActivity.java
@@ -0,0 +1,17 @@
+package org.c_base.c_beam_viewer;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class SettingsActivity extends PreferenceActivity {
+    public static final String KEY_PREF_MQTT_USER = "pref_key_mqtt_user";
+    public static final String KEY_PREF_MQTT_PASSWORD = "pref_key_mqtt_password";
+    public static final String KEY_PREF_MQTT_URI = "pref_key_mqtt_uri";
+    public static final String KEY_PREF_MQTT_TLS = "pref_key_mqtt_tls";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
+    }
+}
