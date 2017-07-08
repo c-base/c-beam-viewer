119ae6f9c23ad7442ebadf7a1ad4b0d4f3e1884a
ligi <ligi@ligi.de>
2017-07-08 22:32:53 +0200

    Automagically start activity on boot

--- app/src/main/java/org/c_base/c_beam_viewer/boot/BootReceiver.java
+++ app/src/main/java/org/c_base/c_beam_viewer/boot/BootReceiver.java
@@ -7,6 +7,8 @@ import android.net.ConnectivityManager;
 
 import org.c_base.c_beam_viewer.CbeamViewerApplication;
 import org.c_base.c_beam_viewer.mqtt.MqttManager;
+import org.c_base.c_beam_viewer.ui.activity.MainActivity;
+import static android.content.Intent.FLAG_ACTIVITY_NEW_TASK;
 
 public class BootReceiver extends BroadcastReceiver {
     public BootReceiver() {
@@ -16,7 +18,9 @@ public class BootReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         String action = intent.getAction();
         if (Intent.ACTION_BOOT_COMPLETED.equals(action)) {
-            startMqttConnection(context);
+            final Intent newActivityIntent = new Intent(context, MainActivity.class);
+            newActivityIntent.addFlags(FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(newActivityIntent);
         } else if (ConnectivityManager.CONNECTIVITY_ACTION.equals(action)) {
             boolean noConnectivity = intent.getBooleanExtra(ConnectivityManager.EXTRA_NO_CONNECTIVITY, false);
             boolean hasConnectivity = !noConnectivity;
